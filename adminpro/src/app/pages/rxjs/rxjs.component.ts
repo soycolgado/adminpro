0771150db01f13129779c5aac46b913cac72c01c
soycolgado <soycolgado@gmail.com>
2020-04-05 22:42:53 -0300

    Terminado retry operator

--- adminpro/src/app/pages/rxjs/rxjs.component.ts
+++ adminpro/src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subscriber } from 'rxjs';
+import { retry } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-rxjs',
@@ -7,9 +10,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RxjsComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+
+    this.regresaObservable().subscribe(
+      (data)=>{
+        console.log(data);
+      },(error) => {
+        console.error('Error: ', error);
+      },()=>{
+        console.log('Observador completado');
+      }
+    );
+    // let obs = new Observable(observer => {
+    //   let contador = 0;
+
+    //   let intervalo = setInterval(()=> {
+        
+    //     contador+=1;
+    //     observer.next(contador);
+        
+    //     if(contador === 3 ){
+    //       clearInterval(intervalo);
+    //       observer.complete();
+    //     }
+  
+    //     if(contador === 2){
+    //       clearInterval(intervalo);
+    //       observer.error('Auxilio');
+    //     }
+
+    //   },1000);
+    // });
+
+    // obs.pipe(
+    //   retry()
+    // ).subscribe(data => {
+    //   console.log('Subs: ' + data);
+    // },error => {
+    //   console.log('Error al recibir la data', error);
+    // },()=>{
+    //   console.log('Ya finalizo el subscribe');
+    // });
+
+  }
 
   ngOnInit() {
+  }
+
+  regresaObservable(): Observable<any>{
+    const obs = new Observable((observer: Subscriber<any>) => {
+        
+      let contador = 0;
+
+      let intervalo = setInterval(()=>{
+        contador+=1;
+        observer.next(contador);
+        
+        if(contador === 3){
+          clearInterval(intervalo);
+          observer.complete();
+        }
+
+        // if(contador === 2){
+        //   observer.error('Finalizado inesperadamente');
+        // }
+
+      },1000);
+  
+
+    });
+
+    return obs;
+
   }
 
 }
