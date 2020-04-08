2b3d60ce45a2bc180a7030e9f100125916433c67
soycolgado <soycolgado@gmail.com>
2020-04-07 21:01:24 -0300

    Terminando primero de observer

--- adminpro/src/app/pages/rxjs/rxjs.component.ts
+++ adminpro/src/app/pages/rxjs/rxjs.component.ts
@@ -11,77 +11,36 @@ import { retry } from 'rxjs/operators';
 export class RxjsComponent implements OnInit {
 
   constructor() { 
-
-    this.regresaObservable().subscribe(
-      (data)=>{
-        console.log(data);
-      },(error) => {
-        console.error('Error: ', error);
-      },()=>{
-        console.log('Observador completado');
-      }
-    );
-    // let obs = new Observable(observer => {
-    //   let contador = 0;
-
-    //   let intervalo = setInterval(()=> {
-        
-    //     contador+=1;
-    //     observer.next(contador);
-        
-    //     if(contador === 3 ){
-    //       clearInterval(intervalo);
-    //       observer.complete();
-    //     }
-  
-    //     if(contador === 2){
-    //       clearInterval(intervalo);
-    //       observer.error('Auxilio');
-    //     }
-
-    //   },1000);
-    // });
-
-    // obs.pipe(
-    //   retry()
-    // ).subscribe(data => {
-    //   console.log('Subs: ' + data);
-    // },error => {
-    //   console.log('Error al recibir la data', error);
-    // },()=>{
-    //   console.log('Ya finalizo el subscribe');
-    // });
-
-  }
-
-  ngOnInit() {
-  }
-
-  regresaObservable(): Observable<any>{
-    const obs = new Observable((observer: Subscriber<any>) => {
-        
+    let obs = new Observable(observer => {
       let contador = 0;
 
-      let intervalo = setInterval(()=>{
-        contador+=1;
+     let interval = setInterval( ()=> {
+        contador++;
         observer.next(contador);
-        
         if(contador === 3){
-          clearInterval(intervalo);
+          clearInterval(interval);
           observer.complete();
         }
 
-        // if(contador === 2){
-        //   observer.error('Finalizado inesperadamente');
-        // }
+        if(contador === 2){
+          observer.error('Auxilio!!!');
+        }
 
-      },1000);
-  
+    },1000);
 
     });
 
-    return obs;
+    obs.subscribe(numero=>{
+      console.log('Subs: ',numero);
+    },error => {
+      console.log(error);
+    },()=>{
+      console.log('Completado!!!!');
+    });
 
   }
+  ngOnInit() {
+  }
+
 
 }
